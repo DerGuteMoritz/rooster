ec2a227b61d6463645a1ff4b2d7471193b8a5379
David Reynolds <david@alwaysmovefast.com>
2010-08-21 19:34:49 -0700

    Update README to show how to run a basic webserver

--- examples/webserver/webserver.scm
+++ examples/webserver/webserver.scm
@@ -1,8 +1,10 @@
 (require 'rooster)
 
-(define headers "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\n")
-
 (define (handler fd buf)
-    (send-to-client fd (string-append headers "Hello, world")))
+    (let* ((msg "Hello, world")
+           (headers (format
+                        "HTTP/1.1 200 OK\r\nContent-Length:~A\r\n\r\n"
+                        (string-length msg))))
+        (send-to-client fd (string-append headers msg))))
 
 (run-rooster handler)
